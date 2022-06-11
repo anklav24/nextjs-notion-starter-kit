85e36e38c042060aae87890f6c4f94a2a03b0dc1
Aleksandr Lobanov <anklav24@gmail.com>
2022-06-11 19:33:53 +0000

    feat: add override /resume url

--- site.config.ts
+++ site.config.ts
@@ -41,11 +41,10 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/resume': 'bfb7f09dce924d1a813058126697a74f'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
