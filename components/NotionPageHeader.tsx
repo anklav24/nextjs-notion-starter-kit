91247a0208b6e1be1351de6876f3dcd3074a8fe1
Aleksandr Lobanov <anklav24@gmail.com>
2022-06-24 19:53:02 +0000

    feat: remove breadcrumps

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -44,8 +44,6 @@ export const NotionPageHeader: React.FC<{
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
-
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
             ?.map((link, index) => {
